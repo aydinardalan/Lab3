54cd80a74b09624217c109709ff048d2687ea5f6
aydinardalan <aydin.ardalan@gmail.com>
2022-09-07 12:03:01 +0200

    addin documentation to data set

---
+++ R/wiki_graph.R
@@ -0,0 +1,13 @@
+#' Wiki Graph
+#' 
+#' A dataset containing the nodes and edges of the graph from the first example on the Wikipedia page of Dijkstra algorithm.
+#' 
+#' @format A dataframe with 3 columns and 18 rows:
+#' \describe{
+#'   \item{v1}{an edge starting from vertex / node 1}
+#'   \item{v2}{to the target vertex / node 2}
+#'   \item{w}{weight of the edge between v1 and v2}
+#' }
+#' @name wiki_graph
+#' @source \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+"wiki_graph"
