0135620134cb96d308a44411c2bcab1e14a03820
aydinardalan <aydin.ardalan@gmail.com>
2022-09-07 18:18:34 +0200

    adding testthat

--- tests/testthat/test-euclidean.R
+++ tests/testthat/test-euclidean.R
@@ -1,3 +1,18 @@
-test_that("multiplication works", {
+# test_that("multiplication works", {
+#   expect_equal(euclidean(123612, 13892347912), 4)
+# })
+
+context("euclidean")
+
+test_that("GDC is calculated correctly.", {
   expect_equal(euclidean(123612, 13892347912), 4)
+  expect_equal(euclidean(100, 1000), 100)
+  expect_equal(euclidean(-100, 1000), 100)
+})
+
+
+test_that("Wrong input throws an error.", {
+  expect_error(euclidean("100", 1000))  
+  expect_error(euclidean(100, "1000"))
+  expect_error(euclidean(TRUE, "1000"))  
 })
