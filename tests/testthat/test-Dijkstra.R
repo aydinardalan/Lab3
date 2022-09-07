b9058f7f129f0cd3790c13359b8692bca1692879
aydinardalan <aydin.ardalan@gmail.com>
2022-09-07 18:23:28 +0200

    Final

--- tests/testthat/test-Dijkstra.R
+++ tests/testthat/test-Dijkstra.R
@@ -1,6 +1,3 @@
-# test_that("multiplication works", {
-#   expect_equal(dijkstra(wiki_graph, 1), c(0,7,9,20,20,11))
-# })
 context("dijkstra")
 
 wiki_graph <-
--- tests/testthat/test-euclidean.R
+++ tests/testthat/test-euclidean.R
@@ -1,7 +1,3 @@
-# test_that("multiplication works", {
-#   expect_equal(euclidean(123612, 13892347912), 4)
-# })
-
 context("euclidean")
 
 test_that("GDC is calculated correctly.", {
